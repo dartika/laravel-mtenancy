543fb17b850a7d8ea512f33f664e4e24c51a7b5f
mbellido <mbellido@dartika.com>
2017-07-28 09:33:02 +0200

    Updated config with doc

--- config/laravel-mtenancy.php
+++ config/laravel-mtenancy.php
@@ -1,5 +1,17 @@
 <?php
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default tenant migrations path
+    |--------------------------------------------------------------------------
+    |
+    | Path for each tenant migration (not the global tenant router), here is
+    | where you should put your custom app migrations and will be populated
+    | to each created tenant.
+    |
+    */
+
     'migrations_path' => 'database/migrations/tenants'
 ];
