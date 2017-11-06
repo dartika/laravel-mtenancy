6606167fa24b4e2f7d53b3a150287884717bed3c
mbellido <mbellido@dartika.com>
2017-11-06 13:42:36 +0100

    Optimized bootstrapping, without tenant testing environment

--- src/TenantBootstrap.php
+++ src/TenantBootstrap.php
@@ -9,43 +9,21 @@ class TenantBootstrap
 {
     public static function boot($app)
     {
-        $tenant = null;
-
-        if ($app->runningInConsole()) {
-            if ($app->environment('testing')) {
-                // temporal tenant for testing
-                $tenant = new Tenant([
-                    'name' => 'testing',
-                    'subdomain' => 'testing',
-                    'dbhost' => config('database.connections.tests.host'),
-                    'dbdatabase' => config('database.connections.tests.database'),
-                    'dbusername' => config('database.connections.tests.username'),
-                    'dbpassword' => config('database.connections.tests.password'),
-                ]);
-
-                $tenant->setActive();
-                $tenant->migrate();
-            }
-        } else {
+        if (!$app->runningInConsole()) {
             $subdomain = self::getTenantSubdomain($app->request);
+            $tenant = Tenant::whereSubdomain($subdomain)->first();
 
-            if (!$app->environment('testing')) {  // testing
-                $tenant = Tenant::whereSubdomain($subdomain)->first();
-            }
-        }
-
-        if ($tenant) {
-            $tenant->setActive();
-        } else {
-            if (!$app->runningInConsole() && !$app->environment('testing')) {
+            if(!$tenant) {
                 abort(404);
             }
+
+            return $tenant->setActive();
         }
     }
 
     protected static function getTenantSubdomain(Request $request)
     {
-        $subdomain = explode('.', $_SERVER['HTTP_HOST'])[0];
+        $subdomain = str_before($request->getHost(), '.'); // TODO: Check subdomain vs domain (empty subdomain). domain config var?
 
         if ($broadcastChannel = $request->get('channel_name')) {  // broadcasting
             preg_match('/^private-private-(.*?)\./', $broadcastChannel, $match);
